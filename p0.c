18382b2f3d380c2fe64a78648be78c2b473b83b6
Javier-r-r <j.rrodriguez1@udc.es>
2023-10-25 19:48:11 +0200

    Stat implementado

--- p0.c
+++ p0.c
@@ -14,6 +14,7 @@
 #include <pwd.h>
 #include <grp.h>
 #include <dirent.h>
+#define _GNU_SOURCE
 #include <stdbool.h>
 #include "comand_list.h"
 
@@ -313,7 +314,7 @@ void Cmd_create(char *tr[]){
     if(mkdir(tr[0],0775) == -1) perror("Ha ocurridon un error, no se pudo crear el directorio");
   }
 }
-/*
+
 //Set 1 to each parameter if it is input
 struct statParams getParams(char *tr[], struct statParams pr){
 
@@ -328,13 +329,115 @@ struct statParams getParams(char *tr[], struct statParams pr){
    return pr;
 }
 
+char *lastDir(char *directories){
+    char *dirs[MAX/2];
+    int i=1;
+    
+    dirs[0]=strtok(directories, "/ ");
+    while((dirs[i]=strtok(NULL, "/ "))!=NULL)
+        i++;
+    if(i==1) return dirs[0]; //only one token
+    else return dirs[i-1];   //otherwise, last one
+}
+
+char LetraTF (mode_t m)
+{
+     switch (m&S_IFMT) { /*and bit a bit con los bits de formato,0170000 */
+        case S_IFSOCK: return 's'; /*socket */
+        case S_IFLNK: return 'l'; /*symbolic link*/
+        case S_IFREG: return '-'; /* fichero normal*/
+        case S_IFBLK: return 'b'; /*block device*/
+        case S_IFDIR: return 'd'; /*directorio */ 
+        case S_IFCHR: return 'c'; /*char device*/
+        case S_IFIFO: return 'p'; /*pipe*/
+        default: return '?'; /*desconocido, no deberia aparecer*/
+     }
+}
+
+
+char * ConvierteModo2 (mode_t m)
+{
+  static char permisos[12];
+  strcpy (permisos,"---------- ");
+    
+  permisos[0]=LetraTF(m);
+  if (m&S_IRUSR) permisos[1]='r';    /*propietario*/
+  if (m&S_IWUSR) permisos[2]='w';
+  if (m&S_IXUSR) permisos[3]='x';
+  if (m&S_IRGRP) permisos[4]='r';    /*grupo*/
+  if (m&S_IWGRP) permisos[5]='w';
+  if (m&S_IXGRP) permisos[6]='x';
+  if (m&S_IROTH) permisos[7]='r';    /*resto*/
+  if (m&S_IWOTH) permisos[8]='w';
+  if (m&S_IXOTH) permisos[9]='x';
+  if (m&S_ISUID) permisos[3]='s';    /*setuid, setgid y stickybit*/
+  if (m&S_ISGID) permisos[6]='s';
+  if (m&S_ISVTX) permisos[9]='t';
+    
+  return permisos;
+}  
+
+void printStats(char *tr, struct statParams *pr){
+    struct stat fs; 
+    // lstad instead of stat because if path is a symbolic link,then the link itslef is stat-ed, not the file    
+    char copy[MAX];
+    strcpy(copy,tr);
+    
+    if(lstat(tr,&fs)){     
+      fprintf(stderr, "****error al acceder a %s: ", tr);
+      perror("");
+      return;
+    }
+    
+    int total= pr->lon + pr->lnk + pr->acc + pr->hid + pr->reca + pr->recb;
+    
+    if(total==0){    	
+        printf("% 8ld  %s\n", fs.st_size,lastDir(tr));
+    }else{
+       	
+    	//-long option
+    	if(pr->lon){
+    	
+    	    struct group *gr;
+            struct passwd *pw;
+            char buffer[MAX];
+            char symBuff[MAX]="";  //initialize to "" for avoiding memory errors
+    
+    	    if((pw=getpwuid(fs.st_uid)) == NULL) {
+            fprintf(stderr, "****error al acceder a %s ", tr);
+            perror("");
+          }
+    	    if((gr=getgrgid(fs.st_gid)) == NULL) {
+            fprintf(stderr, "****error al acceder a %s ", tr);
+            perror("");
+          }
+    	
+    	    //-acc option
+    	    if((pr->acc) == 1) strftime(buffer,MAX,"%Y/%m/%d-%H:%M",localtime(&fs.st_atime));
+    	    else strftime(buffer,MAX,"%Y/%m/%d-%H:%M",localtime(&fs.st_mtime));
+    	 	
+    	     printf("%s   %ld (  %ld)   %s   %s",buffer,fs.st_nlink,fs.st_ino,pw->pw_name,gr->gr_name);
+    	     printf(" %s\t% 8ld %s",ConvierteModo2(fs.st_mode),fs.st_size,lastDir(tr));
+       	    
+    	    //-link option
+    	    if(!pr->lnk){
+    	       printf("\n");
+    	    }else{
+    	       if(pr->lnk && (readlink(copy,symBuff,MAX) != -1))  
+    	          printf(" ->%s\n",symBuff);
+    	       else printf(" ->\n");
+    	    }
+         	    
+        }else printf("% 8ld  %s\n", fs.st_size,lastDir(tr));
+   }
+}
+
 void Cmd_stat(char *tr[]){
 
     char dir[MAX];
     
     if(tr[0] == NULL){
     	printf("%s\n",getcwd(dir,MAX));
-    	printf("Please input a name of a file or directory\n");
     }else{    
       struct statParams pr={0,0,0,0,0,0};
       int counterFiles=0;      
@@ -348,11 +451,10 @@ void Cmd_stat(char *tr[]){
       }
       if(counterFiles==0){ //if we type the command with parameters but there isn't any file
          printf("%s\n",getcwd(dir,MAX));
-         printf("Please input a name of a file or directory\n");
       } 
    }    
 }
-*/
+
 
 void Cmd_delete(char *tr[]){
   char dir[MAX];
@@ -391,7 +493,8 @@ struct cmd cmds[]={
   {"chdir",Cmd_chdir},
   {"help",Cmd_help},
   {"create", Cmd_create},
-  {"delete", Cmd_delete}
+  {"delete", Cmd_delete},
+  {"stat", Cmd_stat},
 };
 
 void procesar_comando(char *tr[], tList comandList, tListF fileList) {
